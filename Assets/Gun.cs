c7e13fd97de2e62977954603c31868135a1e89ec
Martin Spicuzza <spicuzza157@gmail.com>
2020-02-26 19:54:56 -0600

    adding functionality to the PC, added new input controller with the preview unity package

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
+[RequireComponent(typeof(AudioSource))]
 public class Gun : MonoBehaviour
 {
+    public int magazineSize = 30;    // actual size of the magazine
     public int activeAmmoCount = 30; // active amount of in-magazine ammo
-    public int totalAmmoCount = 550; //total amount of ammo the gun has
-    public int maxAmmoCount = 550; // max amount of ammo possible in the gun
-    public int magazineSize = 30;
+    public int totalAmmoCount = 550; // total amount of ammo the gun has in-storage
+    public int maxAmmoCount = 550;   // max amount of ammo possible in the gun 
+    public string gun_name = "Gun Name";
 
-    public void Shoot()
+
+    private AudioSource audioSource;
+
+    private void Awake()
     {
-        if(activeAmmoCount > 0)
+        
+    }
+
+    public virtual void Shoot()
+    {
+        if (activeAmmoCount > 0)
         {
             activeAmmoCount--;
             // instantiate bullet entity here
         }
+        else Reload();
     }
 
-    public void Reload()
+    public virtual void Reload()
     {
         if (totalAmmoCount == 0) return; // play "cannot reload" animation
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,62 @@ using Unity.Entities;
 [RequireComponent(typeof(CapsuleCollider))]
 public class PlayerController : MonoBehaviour
 {
-    
+
+    private Rigidbody rb;
+    public Camera cam;
+
+    //#region Camera Movement System
+    public float camMoveSpeed = 1;
+    public bool cameraBob = false;
+
+    private void MoveCam()
+    {
+        // Mouse look with sensitivity based on camMoveSpeed
+    }
+    //#endregion
+
+    //#region Movement System
+    public bool isMoving = false;
+    public float moveSpeedForward = 1f;
+    public float moveSpeedBackPedal = 1f;
+    public float moveSpeedStrafe = 1f;
+
+    private void MovePlayer()
+    {
+        // move speed based on moveSpeed variable family
+    }
+
+    public float moveSpeedSprint = 2f;
+    private void Sprint()
+    {
+        // increase speed to moveSpeedSprint
+        IEnumerator speedUp()
+        {
+
+            yield return null;
+        }
+        StartCoroutine(speedUp());
+    }
+    //#endregion
+
+    //#region Jump System
+    public bool isGrounded
+    {
+        get {
+            float rayDistance = 0.01f;
+            return Physics.Raycast(transform.position + (Vector3.up * rayDistance), Vector3.down, rayDistance);
+        }
+    }
+    public float jumpForce = 1f;    // force to add to the rigidbody to make it jump
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce);
+    }
+    //#endregion
+
+
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
     }
 }
